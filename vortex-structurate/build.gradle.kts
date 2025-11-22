d8a8e049654d18292790e9cf9c89d2845f2beb72
itspanncake <72684309+itspanncake@users.noreply.github.com>
2025-11-22 20:40:21 +0100

    Add lombok in structurate module

--- vortex-structurate/build.gradle.kts
+++ vortex-structurate/build.gradle.kts
@@ -4,4 +4,7 @@ plugins {
 
 dependencies {
     implementation("org.yaml:snakeyaml:2.5")
+
+    compileOnly("org.projectlombok:lombok:1.18.42")
+    annotationProcessor("org.projectlombok:lombok:1.18.42")
 }
