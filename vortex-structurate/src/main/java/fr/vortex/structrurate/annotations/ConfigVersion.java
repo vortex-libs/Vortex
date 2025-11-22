d481f746b23a741730ee9acff5282edd79fd8d3b
itspanncake <72684309+itspanncake@users.noreply.github.com>
2025-11-22 20:41:13 +0100

    Create all structurate annotations
    Add:
    - @ConfigComment
    - @ConfigConverter
    - @ConfigIgnore
    - @ConfigKey
    - @ConfigVersion
    - @NotNull
    - @Range
    - @Regex

---
+++ vortex-structurate/src/main/java/fr/vortex/structrurate/annotations/ConfigVersion.java
@@ -0,0 +1,12 @@
+package fr.vortex.structrurate.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+public @interface ConfigVersion {
+    int value();
+}
